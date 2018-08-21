58d6c190370d1669e79fa85a2bcfe2d875893ac1
Anders Kvalvik KVERNBERG <anders.kvernberg@soprasteria.com>
2018-08-21 10:41:53 +0200

    Implemented GetLetterGrade in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks {
     class RankedGradeBook : BaseGradeBook{
         public RankedGradeBook(string name) : base(name) {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade) {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var grades = 
+                from student in Students
+                where student.AverageGrade > averageGrade
+                select student.AverageGrade;
+
+            double percentage = 1.0 - ((double)grades.Count() / (double)Students.Count);
+
+            if (percentage > 0.8)
+                return 'A';
+            else if (percentage > 0.6)
+                return 'B';
+            else if (percentage > 0.4)
+                return 'C';
+            else if (percentage > 0.2)
+                return 'D';
+
+            return 'F';
         }
     }
 }
